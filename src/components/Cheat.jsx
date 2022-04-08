c4dcb3ef7219f0119f01001b3daad9e24dbad1a0
ntnoop <jmryuart@naver.com>
2022-04-08 18:51:14 +0900

    로그인 구현

---
+++ src/components/Cheat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cheat = () => {
+  return(
+    <div>
+      Cheat
+    </div>
+  )
+}
+
+export default Cheat;
---
+++ src/components/Introduce.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "../css/Introduce.module.css"
+
+const Introduce = () => {
+  return(
+    <div className={styled.introJinmo}>
+      Introduce
+    </div>
+  )
+}
+
+export default Introduce;
